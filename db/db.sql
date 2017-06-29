e76f6fb88fceca1515928c9cced19593a5dd5d0b
xuzhojun@163.com <xuzhojun@163.com>
2017-06-29 21:17:59 +0800

    db

---
+++ db/db.sql
@@ -0,0 +1,45 @@
+create table t_user (
+	f_username varchar(40) primary key,
+	f_password varchar(256) not null,
+	f_status   int not null
+);
+
+create table t_client (
+	f_acc_no   varchar(20) primary key,
+	f_pbu      varchar(10) not null,
+	f_quantity bigint not null,
+	f_status   int    not null
+);
+
+create table t_trade_info (
+	f_trade_date  varchar(8),
+	f_sge_instruc varchar(16),
+	f_recv_type   int not null,
+	f_trans_no    int not null,
+	f_msg_type    int not null,
+	f_etf_code    varchar(8) not null,
+	f_client_acc  varchar(20) not null,
+	f_pbu         varchar(10) not null,
+	f_quantity    bigint not null,
+	f_result_code varchar(10),
+	f_result_desc varchar(80),
+	primary key(f_trade_date, f_sge_instruc)
+);
+
+create table t_trade_date (
+	f_trade_date  varchar(8) not null
+);
+
+create table t_trade_time (
+	f_seq        int primary key,
+	f_start_time varchar(8) not null,
+	f_end_time   varchar(8) not null
+);
+
+create table t_trade_list (
+	f_trade_date  varchar(8) not null,
+	f_etf_code    varchar(10) not null,
+	f_apply_limit bigint not null,
+	f_redemp_limit bigint not null,
+	primary key(f_trade_date, f_etf_code)
+);
