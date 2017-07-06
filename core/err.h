ca97f0023862de7521cb9c95cdedefab56211187
hu-qing <18976006715@163.com>
2017-07-06 16:13:42 +0800

    Add files via upload

--- core/err.h
+++ core/err.h
@@ -5,5 +5,7 @@
 #define WRONG_PASSWORD  "wrong password"
 #define USER_STATUS_ERR "user status error"
 #define TRADE_DATE_ERR  "trade date error"
+#define APPLY_LIMIT_NOT_FOUND  "apply limit not found"
+#define QUANTITY_ERROR  "quantity is over range"
 
 #endif
